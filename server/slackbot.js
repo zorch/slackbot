9bf8c27ba8e1a90641ac74d48f9de96485cdfd65
zorch <zorch.mcclaud.da@gmail.com>
2016-04-27 09:01:40 -0500

    - Adding correct handles for bot

--- server/slackbot.js
+++
@@ -1,8 +0,0 @@
-import { Meteor } from 'meteor/meteor';
-// {
-//    token: 'xoxb-35992647746-BXVnspq906WcNcdIdKHLKaiM', // Add a bot https://my.slack.com/services/new/bot and put the token
-//    name: 'dailyupdates'
-// }
-Meteor.startup(function() {
-	Meteor.call("start.app");
-});
